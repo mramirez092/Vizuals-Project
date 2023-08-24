8e4e3dae761932de628e996862ed89b28a126cf3
mramirez092 <miguelramirez2210@gmail.com>
2023-08-24 18:18:06 +0000

    Fix m-p

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,19 +1,44 @@
-import React from "react";
+import React, { useState } from 'react';
 import { Link } from "react-router-dom";
+import "../../styles/navbar.css";
+import * as FaIcons from "react-icons/fa";
+import * as AiIcons from "react-icons/ai";
+import { SideBarData } from './sideBarData';
+import { IconContext } from 'react-icons';
 
 export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-				</div>
-			</div>
-		</nav>
-	);
+  const [sidebar, setSideBar] = useState(false)
+
+  const showSideBar = () => setSideBar(!sidebar)
+
+  return (
+    <>
+    <IconContext.Provider value={{ color: 'black' }}>
+    <div className="navbar"> {/* Corrección: className */}
+      <Link to="#" className="menu-bars">
+        <FaIcons.FaBars className='menu-open' onClick={showSideBar}/> {/* Corrección: className */}
+      </Link>
+    </div>
+      <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
+        <ul className='nav-menu-items' onClick={showSideBar}>
+          <li className='navbar-toggle'>
+            <Link to='#' className='menu-bars'>
+              <AiIcons.AiOutlineClose />
+            </Link>
+          </li>
+          {SideBarData.map((item, index) => {
+            return(
+              <li key={index} className={item.cName}>
+                <Link to={item.path}>
+                  {item.icon}
+                  <span>{item.title}</span>
+                </Link>
+              </li>
+            )
+          })}
+        </ul>
+      </nav>
+      </IconContext.Provider>
+      </>
+  );
 };
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,17 +1,17 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import PreLoader from "../component/preLoader";
 import "../../styles/home.css";
 
 export const Home = () => {
 
-	return (
-		<>
-			<>
-				<PreLoader />
-			</>
-			<div className="container">
-				<h1>Hello World.</h1>
-			</div>
-		</>
-	);
+  
+
+  return (
+    <>
+      <PreLoader />
+      <div className="container-fluid"> {/* Agrega la clase main-content aquí */}
+        <h1>Hello World.</h1>
+      </div>
+    </>
+  );
 };
